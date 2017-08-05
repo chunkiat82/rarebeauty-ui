8985e1fb5dbacf0f1b5d9cacccea8f7ddaf2c5a7
Raymond Ho <chunkiat82@gmail.com>
2017-08-05 22:36:29 +0800

    database commands

---
+++ src/data/database/index.js
@@ -0,0 +1,56 @@
+const couchbase = require('couchbase');
+
+const cluster = new couchbase.Cluster('couchbase://rarebeauty.soho.sg/');
+
+export async function upsert(id, doc) {
+  // console.log(id);
+  // console.log(doc);
+  const obj = await runOperation(setObject, { id, doc });
+  return obj;
+}
+export async function get(id) {
+  const obj = await runOperation(getObject, { id });
+  return obj;
+}
+
+async function runOperation(operation, options) {
+  const bucket = cluster.openBucket('default');
+  let res = null;
+  try {
+    res = await operation(bucket, options);
+  } catch (err) {
+    throw err;
+  }
+  bucket.disconnect();
+  return res;
+}
+
+function getObject(bucket, options) {
+  const { id } = options;
+  return new Promise((res, rej) => {
+    bucket.get(id, (err, result) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(result);
+      }
+    });
+  });
+}
+
+function setObject(bucket, options) {
+  const { id, doc } = options;
+  return new Promise((res, rej) => {
+    bucket.upsert(id, doc, (err, result) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(result);
+      }
+    });
+  });
+}
+export default {
+  upsert,
+  get,
+};
