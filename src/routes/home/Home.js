490d2f5a4fb2c42ca0b8be59845721826a55258e
Raymond Ho <chunkiat82@gmail.com>
2017-08-12 15:03:28 +0800

    home page up

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -10,202 +10,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import DatePicker from 'material-ui/DatePicker';
-import TimePicker from 'material-ui/TimePicker';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
-import Slider from 'material-ui/Slider';
-import FontIcon from 'material-ui/FontIcon';
-import RaisedButton from 'material-ui/RaisedButton';
-import AutoComplete from 'material-ui/AutoComplete';
-import Snackbar from 'material-ui/Snackbar';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import s from './Home.css';
+import Link from '../../components/Link/Link';
 
-const listOfServices = [
-  'Full Set',
-  'Touch Up',
-  'Removal',
-  'Radiance Facial',
-  'Brazilian Waxing',
-  'Full Leg Waxing',
-  'Half Leg Waxing',
-  'Full Arm Waxing',
-  'Half Arm Waxing',
-  'Under Arm Waxing',
-  'Full Face Waxing',
-  'Lower Lip Waxing',
-  'Eyebrow Threading',
-  'Lower Lip Threading',
-  'Upper Lip Threading',
-  'Full Face Threading',
-];
-
-const iconStyles = {
-  marginRight: 24,
-  verticalAlign: 'middle',
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        width: 500,
+        height: 250,
+        overflowY: 'auto',
+    },
 };
 
+const tilesData = [
+    {
+        img: 'images/home/empty-calendar.png',
+        title: 'Create Appointment',
+        subTitle: 'With/Without Payments',
+        link: '/appointment/create'
+    },
+    {
+        img: 'images/home/calendar-appointments.png',
+        title: 'List Appointments',
+        subTitle: 'Upcoming 20',
+        link: '/appointment/list'
+    }
+];
+
 class Home extends React.Component {
-  static propTypes = {
-    contact: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        mobile: PropTypes.string.isRequired,
-        resourceName: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
-    post: PropTypes.func.isRequired,
-  };
+    static propTypes = {
+    };
 
-  state = {
-    open: false,
-    slider: 75,
-  };
-
-  handleNewRequest = (inputString, index) =>
-    this.setState({ index, nameInput: '', mobileInput: '', ...inputString });
-  handleServiceChange = (event, index, services) => {
-    this.setState({ services });
-  };
-  handleSliderChange = (event, value) => this.setState({ slider: value });
-  handleDateChange = (something, dateChosen) => this.setState({ dateChosen });
-  handleTimeChange = (something, timeChosen) => this.setState({ timeChosen });
-  handleUpdateName = nameInput =>
-    this.setState({ nameInput, resourceName: '' });
-  handleUpdateMobile = mobileInput =>
-    this.setState({ mobileInput, resourceName: '' });
-
-  render() {
-    const { services } = this.state;
-    // console.log(this.state);
-    return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <AutoComplete
-            ref={c => {
-              this.nameAC = c;
-            }}
-            hintText="Type anything"
-            dataSource={this.props.contact}
-            dataSourceConfig={{ text: 'display', value: 'name' }}
-            onNewRequest={this.handleNewRequest}
-            onUpdateInput={this.handleUpdateName}
-            floatingLabelText="Name"
-            filter={AutoComplete.fuzzyFilter}
-            fullWidth
-            searchText={this.state.name}
-          />
-          <AutoComplete
-            ref={c => {
-              this.mobileAC = c;
-            }}
-            hintText="Type anything"
-            dataSource={this.props.contact}
-            dataSourceConfig={{ text: 'mobile', value: 'mobile' }}
-            onNewRequest={this.handleNewRequest}
-            floatingLabelText="Mobile"
-            onUpdateInput={this.handleUpdateMobile}
-            filter={AutoComplete.fuzzyFilter}
-            fullWidth
-            searchText={this.state.mobile}
-          />
-          <DatePicker
-            hintText="Date"
-            autoOk
-            fullWidth
-            onChange={this.handleDateChange}
-            value={this.state.dateChosen}
-          />
-          <TimePicker
-            autoOk
-            format="24hr"
-            hintText="Time"
-            minutesStep={5}
-            onChange={this.handleTimeChange}
-            fullWidth
-            value={this.state.timeChosen}
-          />
-          <p>
-            <FontIcon className="material-icons" style={iconStyles}>
-              schedule
-            </FontIcon>
-            <span>
-              {'Duration: '}
-            </span>
-            <span>
-              {this.state.slider}
-            </span>
-          </p>
-          <Slider
-            defaultValue={75}
-            step={5}
-            value={this.state.slider}
-            min={5}
-            max={500}
-            onChange={this.handleSliderChange}
-          />
-          <SelectField
-            multiple
-            hintText="Services"
-            value={services}
-            onChange={this.handleServiceChange}
-            fullWidth
-          >
-            {listOfServices.map(name =>
-              <MenuItem
-                key={name}
-                insetChildren
-                checked={
-                  this.state.services && this.state.services.indexOf(name) > -1
-                }
-                value={name}
-                primaryText={name}
-              />,
-            )}
-          </SelectField>
-          <RaisedButton
-            ref={c => {
-              this.submitBtn = c;
-            }}
-            label="Create Appointment"
-            primary
-            fullWidth
-            onClick={() => {
-              const inputs = Object.assign({}, this.state);
-              inputs.mobile = inputs.mobileInput || inputs.mobile;
-              inputs.name = inputs.nameInput || inputs.name;
-
-              this.props.post(inputs);
-              this.setState({
-                open: true,
-                name: '',
-                duration: 75,
-                mobile: '',
-                timeChosen: {},
-                dateChosen: {},
-                services: [],
-                nameInput: '',
-                mobileInput: '',
-                resourceName: '',
-              });
-              this.nameAC.setState({ searchText: '' });
-              this.mobileAC.setState({ searchText: '' });
-              setTimeout(() => this.nameAC.focus(), 200);
-              setTimeout(() => this.setState({ open: false }), 2000);
-            }}
-          />
-          <Snackbar
-            open={this.state.open}
-            message="Appointment Added"
-            bodyStyle={{
-              backgroundColor: '#373277',
-              paddingBottom: 28,
-              paddingTop: 28,
-            }}
-          />
+    render() {
+        return <div style={styles.root}>
+            <GridList
+                cellHeight={180}
+                style={styles.gridList}
+            >
+                <Subheader>How can I help you, Today? Create An Appointment or show all of them?</Subheader>
+                {tilesData.map((tile) => (
+                    <Link key={tile.link} to={tile.link}>
+                        <GridTile
+                            key={tile.img}
+                            title={tile.title}
+                            subtitle={<span>{tile.subTitle}</span>}
+                            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}                           
+                        >
+                            <img src={tile.img} />
+                        </GridTile>
+                    </Link>
+                ))}
+            </GridList>
         </div>
-      </div>
-    );
-  }
+    }
 }
 export default withStyles(s)(Home);
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -12,72 +12,14 @@ import moment from 'moment';
 import Home from './Home';
 import Layout from '../../components/Layout';
 
-async function createCalendar(fetch, input) {
-  const {
-    slider: duration,
-    name,
-    mobile,
-    resourceName = '',
-    timeChosen,
-    dateChosen,
-    services,
-  } = input;
-
-  const dateInput = moment(dateChosen).format('YYYYMMDD');
-  const timeInput = moment(timeChosen).format('HHmm');
-
-  const resp = await fetch('/graphql', {
-    body: JSON.stringify({
-      query: `mutation($name: String!, $mobile:String!, $resourceName:String, $start:String!, $services:[String]!, $duration:Int!) {
-          createEvent(name:$name, mobile:$mobile, resourceName:$resourceName, start:$start, services:$services, duration:$duration ) {
-            name
-            mobile
-            start
-            services
-            duration
-          }
-        }`,
-      variables: JSON.stringify({
-        name,
-        mobile,
-        resourceName,
-        start: `${dateInput}T${timeInput}`,
-        services,
-        duration,
-      }),
-    }),
-  });
-
-  const { data } = await resp.json();
-
-  return data;
-}
-
-async function listContacts(fetch) {
-  const resp = await fetch('/graphql', {
-    body: JSON.stringify({
-      query: '{contact{name,mobile,display,resourceName}}',
-    }),
-  });
-  const { data } = await resp.json();
-
-  return data;
-}
-
 async function action({ fetch }) {
-  const data = await listContacts(fetch);
 
-  if (!data || !data.contact)
-    throw new Error('Failed to load the contact feed.');
   return {
     chunks: ['home'],
     title: 'Rare Beauty Professional',
     component: (
       <Layout>
-        <Home
-          contact={data.contact}
-          post={input => createCalendar(fetch, input)}
-        />
+        <Home/>
       </Layout>
     ),
   };
