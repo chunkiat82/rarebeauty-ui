3464a39eac6d99dd2c1bc3ce599c00f2683fc654
Raymond Ho <chunkiat82@gmail.com>
2019-02-24 21:20:49 +0800

    moving from google shortlink to bitly

--- src/api/urlshortener/create.js
+++ src/api/urlshortener/create.js
@@ -1,15 +1,19 @@
 import rp from 'request-promise';
 
-const key = require('../keys/google.json');
+const { bitly_token } = require('../keys/google.json');
 
 export default async function create(options) {
   const { longURL } = options;
 
   const postOptions = {
     method: 'POST',
-    uri: `https://www.googleapis.com/urlshortener/v1/url?key=${key.api_key}`,
+    headers: {
+      // eslint-disable-next-line camelcase
+      Authorization: `Bearer ${bitly_token}`,
+    },
+    uri: `https://api-ssl.bitly.com/v4/bitlinks`,
     body: {
-      longUrl: longURL,
+      long_url: longURL,
     },
     json: true, // Automatically stringifies the body to JSON
   };
