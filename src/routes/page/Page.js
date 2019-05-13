c20956b50fa7d88af70d2ad714f1a7b55ccc0dd5
Raymond Ho <chunkiat82@gmail.com>
2019-05-13 08:26:10 +0800

    more colourful changes but no structure yet

--- src/routes/page/Page.js
+++ src/routes/page/Page.js
@@ -19,9 +19,9 @@ const style = {
 const aStyle = {
   margin: '3vw',
   width: '20%',
-  minWidth: '120px',
+  minWidth: '100px',
   height: '20%',
-  minHeight: '120px',
+  minHeight: '100px',
 };
 
 const mapStyle = {
@@ -40,12 +40,16 @@ class Page extends React.Component {
   render() {
     return (
       <div className={s.root}>
-        <div style={{ maxWidth: '1280px', margin: '0 auto' }}>
+        <div style={{ maxWidth: '1280px', margin: '0 auto', padding: '5px' }}>
           <span style={{ fontSize: 'calc(12px + 0.75vw)' }}>
-            Providing Eyelash Extensions, Facial, Threading and Waxing Services.<br />
+            Providing{' '}
+            <h4 className={s.rarebeauty}>
+              Eyelash Extensions, Facial, Threading and Waxing Services
+            </h4>
             More than 10 years experience in the beauty industry. <br />
             Staying beautiful inside and outside is our passion.
           </span>
+          <br />
           <Paper style={style} zDepth={5}>
             <img
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/rbsg-rect-page-front.jpg"
@@ -56,39 +60,34 @@ class Page extends React.Component {
           <div style={{ width: '80%', margin: '0 auto' }}>
             <Avatar
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/20190504_3.jpg"
-              size={'20vw'}
               style={aStyle}
             />
             <Avatar
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/20180912.jpg"
-              size={'20vw'}
               style={aStyle}
             />
             <Avatar
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/20181123.jpg"
-              size={'20vw'}
               style={aStyle}
             />
             <Avatar
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/20190421.jpg"
-              size={'20vw'}
               style={aStyle}
             />
             <Avatar
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/dermaspa.jpg"
-              size={'20vw'}
               style={aStyle}
             />
             <Avatar
               src="https://s3-ap-southeast-1.amazonaws.com/rarebeautysg/20190314.jpg"
-              size={'20vw'}
               style={aStyle}
             />
           </div>
           <br />
           <span style={{ fontSize: 'calc(12px + 0.75vw)' }}>
-            We are located near Pioneer MRT/Jurong West Stadium.
+            Located near Pioneer MRT/Jurong West Stadium.
           </span>
+          <br />
           <Paper style={mapStyle} zDepth={5}>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7325944158874!2d103.69252531574084!3d1.336690999025084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da0f8ffc39bba3%3A0x86c5239af2cbccd7!2sSingapore+642987!5e0!3m2!1sen!2ssg!4v1557561471316!5m2!1sen!2ssg"
