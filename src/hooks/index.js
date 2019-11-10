6b3f25e4a65156b828a0fcc48cc906f39c7b279f
Raymond Ho <chunkiat82@gmail.com>
2019-11-10 23:40:41 +0800

    fixing the google webhook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ import { handleCalendarWebhook } from './google';
 import { handleTwilioWebhook } from './twilio';
 
 export { handleCalendarWebhook, handleTwilioWebhook };
+export default { handleCalendarWebhook, handleTwilioWebhook };
