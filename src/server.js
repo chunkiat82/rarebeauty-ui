0cad9bcd3be347205301cf7ca85c234ca353c096
Raymond Ho <chunkiat82@gmail.com>
2022-01-02 13:29:12 +0800

    allow orign works

--- src/server.js
+++ src/server.js
@@ -74,10 +74,6 @@ function populatePayload(req, _res, next) {
 // super temporary lin #######
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
