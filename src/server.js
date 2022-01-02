9cab886445bcf38089edada1ca5c1e1b01471203
Raymond Ho <chunkiat82@gmail.com>
2022-01-02 16:45:36 +0800

    allow origin from different soho servers

--- src/server.js
+++ src/server.js
@@ -73,7 +73,7 @@ function populatePayload(req, _res, next) {
 
 // super temporary lin #######
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://*.soho.sg');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
