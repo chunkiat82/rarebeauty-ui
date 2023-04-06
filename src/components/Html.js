91f2031295e20b778cf41f3fc99f4574df3b6f25
Raymond Ho <chunkiat82@gmail.com>
2023-04-06 13:40:16 +0800

    adding google tag

--- src/components/Html.js
+++ src/components/Html.js
@@ -43,6 +43,10 @@ class Html extends React.Component {
           <title>{title}</title>
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="google-site-verification"
+            content="S0wOAQMshpJMydw4vKy8IQms30yw1FIkzIY6-X6y4oM"
+          />
           {scripts.map(script => (
             <link key={script} rel="preload" href={script} as="script" />
           ))}
